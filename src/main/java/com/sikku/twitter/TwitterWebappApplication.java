606276bb4a47ec7b374636362d9db0974c036632
Upendra <upendrasatna@gmail.com>
2024-02-01 13:40:43 +0530

    project created

---
+++ src/main/java/com/sikku/twitter/TwitterWebappApplication.java
@@ -0,0 +1,13 @@
+package com.sikku.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterWebappApplication.class, args);
+	}
+
+}
